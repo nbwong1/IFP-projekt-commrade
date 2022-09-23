7192c864229e16b5e3be089f12160bcfa4bc9b37
Kevin Umayam <kevin.m.umayam@gmail.com>
2022-09-22 21:18:58 -0700

    got homeroute working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -74,7 +74,7 @@ router.get("/login", (req, res) => {
 router.get("/signup", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/dashboard");
+    res.redirect("/");
     return;
   }
 
