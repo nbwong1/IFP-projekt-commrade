3ff3b3e9c7227220790cf0bbeac965b3112f92cc
nbwong <nicholas.b.wong@gmail.com>
2022-09-25 14:57:09 -0700

    I have updated the comment-route and post-route files.

--- controllers/api/comment-route.js
+++ controllers/api/comment-route.js
@@ -1,3 +1,38 @@
 const router = require("express").Router();
+const { Post, Comment } = require("../../models");
+const withAuth = require('../../utils/auth');
+
+// post route for comments
+router.post('/', async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body, 
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newComment);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try { 
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!'});
+            return;
+        }
+    res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
--- controllers/api/post-route.js
+++ controllers/api/post-route.js
@@ -1,4 +1,41 @@
 const router = require("express").Router();
 const { Post, Expiration } = require("../../models");
+const withAuth = require('../../utils/auth');
+
+// post route for posts (maybe we should name this something else so there's no confusion)
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPost);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+// delete routes for posts
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!'});
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// delete based on Date of event
 
 module.exports = router;
