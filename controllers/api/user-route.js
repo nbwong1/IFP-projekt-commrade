22cfaa731dbcc2adac69c90269b1c49335bc2593
Satorrid <Devintsefton@gmail.com>
2022-09-25 19:22:29 -0700

    css changes

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -31,4 +31,16 @@ router.post("/login", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+//logout
+
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
