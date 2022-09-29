1a9f35499b49e9f451e4c6dc3d5390abd93bcab3
Satorrid <Devintsefton@gmail.com>
2022-09-29 13:06:12 -0700

    finished api and interested button

--- models/Interest.js
+++ models/Interest.js
@@ -11,8 +11,21 @@ Interest.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    interest: {
-      type: DataTypes.BOOLEAN,
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
     },
   },
   {
